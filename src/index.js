87ebf0a6d598444dde2b897fc9028bf61617c5df
Tiago Almeida <tiagoterenas@gmail.com>
2024-12-11 19:57:34 +0000

    Fix Run

--- src/index.js
+++ src/index.js
@@ -6,16 +6,20 @@ import release from "./release.js";
 
 const commands = { issues, milestone, release };
 
-try {
-    const [[command, ...args], options] = parse(core.getInput("command").trim());
-    globalThis.context = JSON.parse(core.getInput("context"));
-    globalThis.repository = core.getInput("repository");
-    globalThis.ref = core.getInput("ref");
-    globalThis.token = core.getInput("token");
-    if (!commands[command]) throw new Error(`Unknown command: ${command}`);
-    const result = commands[command](options, ...args);
-    if (result instanceof Promise) await result;
-    core.setOutput("result", result);
-} catch (error) {
-    core.setFailed(error.message);
+async function run() {
+    try {
+        const [[command, ...args], options] = parse(core.getInput("command").trim());
+        globalThis.context = JSON.parse(core.getInput("context"));
+        globalThis.repository = core.getInput("repository");
+        globalThis.ref = core.getInput("ref");
+        globalThis.token = core.getInput("token");
+        if (!commands[command]) throw new Error(`Unknown command: ${command}`);
+        const result = commands[command](options, ...args);
+        if (result instanceof Promise) await result;
+        core.setOutput("result", result);
+    } catch (error) {
+        core.setFailed(error.message);
+    }
 }
+
+run();
