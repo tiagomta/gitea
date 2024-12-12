295f0cdd0a9b9ae6a9fee6edf7308f73a7601c71
Tiago Almeida <tiagoterenas@gmail.com>
2024-12-12 17:52:01 +0000

    remove logs

--- src/release.js
+++ src/release.js
@@ -9,7 +9,6 @@ export default main;
 const actions = {
     async create(options, tag) {
       if (!tag) throw new Error("No tag specified");
-      console.log(options.milestone);
       const response = await fetch(`${context.api_url}/repos/${repository}/releases`, {
         method: "POST",
         headers: {
@@ -31,7 +30,6 @@ const actions = {
 }
 
 async function getMilestoneInfo(milestone) {
-  console.log(`${context.api_url}/repos/${repository}/issues?state=all&milestone=${milestone}`);
   const response = await fetch(`${context.api_url}/repos/${repository}/issues?state=all&milestone=${milestone}`, {
     method: "GET",
     headers: {
@@ -44,7 +42,6 @@ async function getMilestoneInfo(milestone) {
     throw new Error(`Error getting issues from milestone ${milestone}: ${response.statusText}`);
   
   const issues = await response.json();
-  console.log(issues);
   const types = {};
   for (let i = 0, len = issues.length; i < len; i++) {
     const { number, title, labels: [{ name }] } = issues[i];
